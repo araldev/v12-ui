6d428d3ff7f4d0c8445e4ee13c4eacd00fab7967
araldev <arturo.r2d2.dev@gmail.com>
2025-07-20 23:33:01 +0200

    new layout component

---
+++ src/Stack/Stack.stories.tsx
@@ -0,0 +1,83 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { Stack } from './Stack'
+import type { ComponentPropsWithRef } from 'react'
+
+const meta = {
+  title: 'Components/Stack',
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered'
+  },
+  component: Stack,
+  argTypes: {
+    childrenQuantity: {
+      type: 'number',
+      description: 'Number of children to render',
+      control: 'number',
+      defaultValue: 3
+    },
+    children: {
+      type: 'symbol',
+      description: 'The Stack content',
+      control: false,
+      defaultValue: 'div'
+    },
+    as: {
+      type: 'string',
+      description: 'HTMLElement',
+      control: 'text'
+    },
+    direction: {
+      type: 'string',
+      description: 'Chose the direction of inner items',
+      options: ['row', 'column', 'row-reverse', 'column-reverse'],
+      control: 'select',
+      defaultValue: 'row'
+    },
+    spacing: {
+      type: 'number',
+      description: 'Spacing between items',
+      control: 'number',
+      defaultValue: 4
+    },
+    className: {
+      type: 'string',
+      description: 'Add your own classes',
+      control: false
+    }
+  }
+} satisfies Meta<InnerComponentAndStackProps>
+
+type InnerComponentAndStackProps = ComponentPropsWithRef<typeof Stack> & {
+  childrenQuantity?: number
+}
+
+export default meta
+type Story = StoryObj<InnerComponentAndStackProps>
+
+const InnerComponent: Story = {
+  render: ({ childrenQuantity, ...props }: InnerComponentAndStackProps) => {
+    return (
+      <Stack {...props}>
+        {Array.from({ length: childrenQuantity || 3 }, (_, i) => i + 1).map(Component => (
+          <div key={Component} className='w-32 h-32 font-bold text-3xl flex items-center justify-center bg-amber-950 text-white p-4 rounded'>
+            Item {Component}
+          </div>
+        )
+        )}
+      </Stack>
+    )
+  }
+}
+
+export const Default: Story = {
+  ...InnerComponent,
+  args: {
+    childrenQuantity: 3,
+    as: 'div',
+    direction: 'row',
+    spacing: 4,
+    className: ''
+  }
+}
---
+++ src/Stack/Stack.tsx
@@ -0,0 +1,47 @@
+import { forwardRef } from 'react'
+import type { ReactElement, ComponentPropsWithRef, ElementType, Ref } from 'react'
+import type { WithoutAs } from '../utils/polymorphicTypes'
+import { cn } from '../utils/utils'
+
+type AllowedTags = 'div' | 'span' | 'section' | 'article' | 'nav' | 'aside'
+
+type PolymorphicProps<T extends ElementType = 'div'> = {
+  as?: T
+  children?: ReactElement | ReactElement[]
+  direction?: 'row' | 'column' | 'row-reverse' | 'column-reverse'
+  spacing?: number
+  className?: string
+} & WithoutAs<ComponentPropsWithRef<T>>
+
+const StackInner = <T extends ElementType = 'div'>(
+  {
+    as,
+    children,
+    direction = 'row',
+    spacing = 4,
+    className,
+    ...props
+  }: PolymorphicProps<T>,
+  ref: Ref<T>
+): ReactElement => {
+  const Component = as || 'div'
+
+  return (
+    <Component
+      ref={ref}
+      className={cn(
+        'flex flex-wrap',
+        direction != null && `flex-${String(direction)}`,
+        spacing != null && `gap-${String(spacing)}`,
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </Component>
+  )
+}
+
+export const Stack = forwardRef(StackInner) as <T extends AllowedTags = 'div'>(
+  props: PolymorphicProps<T> & { ref?: Ref<T> }
+) => ReactElement
