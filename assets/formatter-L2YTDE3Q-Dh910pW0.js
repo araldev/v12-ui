e5af1e3a4104234706623e682902ff52514a3787
araldev <araldev@users.noreply.github.com>
2026-06-23 00:14:34 +0000

    deploy: 1f5646b524e739a87f80e6fa1e322a6f6e920198

---
+++ assets/formatter-L2YTDE3Q-Dh910pW0.js
@@ -0,0 +1,1 @@
+import{d as m,r as e}from"./DocsRenderer-JROSPFPF-Dw7aSiUh.js";import{b as o}from"./iframe-BXHUGQXh.js";import"./preload-helper-D9Z9MdNV.js";import"./react-18-Bo0Ghuel.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
